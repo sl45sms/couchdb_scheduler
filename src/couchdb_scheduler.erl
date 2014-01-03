e87e3022247e9141c90feb9327efbb833a6723dd
Panagiotis Skarvelis <sl45sms@gmail.com>
2014-01-04 00:20:28 +0200

    handle iso8601

--- src/couchdb_scheduler.erl
+++ src/couchdb_scheduler.erl
@@ -8,30 +8,34 @@ ensure_schedules_db_exists(DbName, Options) ->
     Options1 = [{user_ctx, #user_ctx{roles=[<<"_admin">>]}}, nologifmissing | Options],
     case couch_db:open(DbName, Options1) of
     {ok, Db} ->
-     ?LOG_INFO("DB already exists: ~p", [DbName]),
+     ?LOG_DEBUG("DB already exists: ~p", [DbName]),
      {ok, Db};
     _Error ->
      {ok, Db} = couch_db:create(DbName, Options1), 
-     ?LOG_INFO("DB created: ~p", [DbName]), 
+     ?LOG_DEBUG("DB created: ~p", [DbName]), 
      {ok, Db}
     end.
 
 open_schedules_db() ->
     DbName = ?l2b(couch_config:get("couchdb_scheduler", "schedules_db","schedules")),%can't name it _schedules underscore is limited only for _users _replicator                                                                                      %so the only way to workarround is to use db without underscore...
-    {ok, AuthDb} =  ensure_schedules_db_exists(DbName, []),
-    AuthDb.
+    {ok, SchedulesDb} =  ensure_schedules_db_exists(DbName, []),
+    SchedulesDb.
 
-create_schedule(DbName,DDocName,ScheduleFunName,DocID,ScheduleTime) ->
+create_schedule(_,_,_,_,[],Acc)->
+                Acc;
+create_schedule(DbName,DDocName,ScheduleFunName,DocID,ScheduleTime,Acc) ->
+               [RS|REST]=ScheduleTime,
+               IsoTime=iso8601:format(RS),
+               {ok,TaskUUID,ScheduleIsoTime}=create_schedule(DbName,DDocName,ScheduleFunName,DocID,IsoTime),
+         create_schedule(DbName,DDocName,ScheduleFunName,DocID,REST,lists:append(Acc,[{uuid,TaskUUID},{isodate,ScheduleIsoTime}])).
+
+create_schedule(DbName,DDocName,ScheduleFunName,DocID,ScheduleIsoTime)->
     Db=open_schedules_db(),
     TaskID=couch_uuids:random(),
+    CouchDB_UUID = ?l2b(couch_config:get("couchdb", "uuid")),
     try 
-       % {A1,A2,A3} = now(),%todo na sozo to creation time...
-
-        ScheduleID=?l2b([<<"schedule:">>,DbName,<<":">>,DDocName,<<":">>,ScheduleFunName,<<":">>,DocID,<<":at:">>,ScheduleTime]),%TODO pio poliploko...
-  
-     %TODO na vazo mesa sto NewDoc kai to UID ths vashs sto trexon mhxanima oste na kserei o skeduler oti prepei na treksei se auto to mhxanima
-     %na mporei na pernei kai parametrika _ oste opia mhxanh prolavei prolave...
-      
+        CreationIsoTime=iso8601:format(now()),%TODO ScheduleIsoTime>CreationIsoTime or throw error
+        ScheduleID=?l2b([<<"schedule:">>,DbName,<<":">>,DDocName,<<":">>,ScheduleFunName,<<":">>,DocID,<<":at:">>,ScheduleIsoTime]),%TODO simpler? more complex?      
         NewDoc = #doc{
           id=ScheduleID,
           body={[
@@ -40,8 +44,11 @@ create_schedule(DbName,DDocName,ScheduleFunName,DocID,ScheduleTime) ->
                  {<<"db">>,DbName},
                  {<<"ddoc">>,DDocName },
                  {<<"fun">>,ScheduleFunName },
-                 {<<"datetime">>,ScheduleTime },
-                 {<<"doc">>, DocID}
+                 {<<"schedule_time">>,ScheduleIsoTime },
+                 {<<"creation_time">>,CreationIsoTime },
+                 {<<"doc">>, DocID},
+                 {<<"performed">>, false},
+                 {<<"couchdb_uuid">>, CouchDB_UUID} %daemon check this 
                 ]}
          },
 
@@ -49,7 +56,7 @@ create_schedule(DbName,DDocName,ScheduleFunName,DocID,ScheduleTime) ->
         case couch_db:update_doc(DbWithoutValidationFunc, NewDoc, []) of
             {ok, _} ->
                 ?LOG_DEBUG("Schedule fun created for ~p:~p", [ScheduleFunName, DocID]),
-                {ok, DocID, ScheduleTime};
+                {ok, TaskID, ScheduleIsoTime};
             Error ->
                 ?LOG_ERROR("Could not create schedule for ~p:~p Reason:", [ScheduleFunName, DocID, Error]),
                 throw(could_not_create_schedule)
@@ -63,12 +70,10 @@ create_schedule(DbName,DDocName,ScheduleFunName,DocID,ScheduleTime) ->
 handle_schedules(#httpd{
         path_parts=[DbName, _, DDocName, _, ScheduleFunName,ScheduleTime,DocID]
     }=Req, _, _) ->
-      %TODO docid optional....
-      %TODO parse ScheduleTime complete... %have to fix the iso8601 lib first
-      [{sign,Sign},{years,Years},{months,Months},
-     {days,Days},{hours,Hours},{minutes,Minutes},
-     {seconds,Seconds}]=iso8601:parse_durations(ScheduleTime),
-      Nm=list_to_integer(Minutes),
-      IsoDate=iso8601:format(iso8601:add_time(calendar:now_to_datetime(now()),1 , Nm, 1)),
-      {_,TaskID,_}=create_schedule(DbName,DDocName,ScheduleFunName,DocID,IsoDate),
-       couch_httpd:send_json(Req, 200, {[{ok, <<"Scheduled">>},{taskid,TaskID},{at,IsoDate}]}).
+      %TODO? docid optional....
+      ScheduleList = case iso8601:is_datetime(ScheduleTime) of
+           true-> [iso8601:parse(ScheduleTime)];
+           false->iso8601:parse_interval(ScheduleTime)
+                       end,
+     TaskIDS=create_schedule(DbName,DDocName,ScheduleFunName,DocID,ScheduleList,[]),
+     couch_httpd:send_json(Req, 200,{[{ok,<<"Scheduled">>},{docid,DocID},{tasks,{TaskIDS}}]}).
